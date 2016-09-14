f700d735d3718ab7b37ef929ddedf83b5045b6eb
ys305751572 <305751572@qq.com>
2016-09-14 10:23:40 +0800

    1

--- src/main/java/com/smallchill/common/base/BaseController.java
+++ src/main/java/com/smallchill/common/base/BaseController.java
@@ -3,7 +3,7 @@ package com.smallchill.common.base;
 import com.smallchill.core.base.controller.BladeController;
 
 /**
- * 用于拓展controller类2
+ * 用于拓展controller类
  */
 public class BaseController extends BladeController {
 
